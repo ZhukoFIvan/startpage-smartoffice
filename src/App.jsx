d2c71ddf148dcd4fae623367c2fa5010e59f682a
ZhukoFIvan <120339888+ZhukoFIvan@users.noreply.github.com>
2023-11-25 13:49:43 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,11 @@ const App = () => {
 			<div className='btns'>
 				<a href='http://91.107.125.49/'>Начать</a>
 				<a href='https://reoffice-contacts.vercel.app/'>Контакты</a>
-				<a href='/src/assets/organized.pdf' download='proposed_file_name'>
+				<a href='/src/assets/organized.pdf'>
 					Презентация
 				</a>
 				<a
 					href='/src/assets/Паспорт Стартап-проекта_MISIS PLUS.docx'
-					download='proposed_file_name'
 				>
 					Паспорт
 				</a>
